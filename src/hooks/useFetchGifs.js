057b4e62d7ed8aa24b364f4f28014bb5837acaee
Leon Matias <rleonm@proveedor.intercorp.com.pe>
2022-01-25 21:46:30 -0500

    Animacion por CSS

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -10,22 +10,12 @@ export const useFetchGifs = (category) => {
     useEffect(() => {
         getGifs(category)
             .then(imgs => {
-                setTimeout(() => {
-                    console.log(imgs);
-                    setState({
-                        data: imgs,
-                        loading: false
-                    });
-
-                }, 3000);
+                setState({
+                    data: imgs,
+                    loading: false
+                });
             })
     }, [category])
 
-    // setTimeout(() =>{
-    //    setState({
-    //        data: [1,2,3,4,5,6,7],
-    //        loading: false
-    //    })
-    // }, 3000 );
     return state;
 }
